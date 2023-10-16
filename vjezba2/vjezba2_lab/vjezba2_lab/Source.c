d69b21a7da878a1934e6cfb2b6cd321394728890
marinmesaric <mmesar00@fesb.hr>
2023-10-16 18:21:26 +0200

    Begining of a second task!

---
+++ vjezba2/vjezba2_lab/vjezba2_lab/Source.c
@@ -0,0 +1,28 @@
+/*
+2. Definirati strukturu osoba (ime, prezime, godina ro?enja) i napisati program koji:
+A.dinami?ki dodaje novi element na po?etak liste,
+B.ispisuje listu,
+C.dinami?ki dodaje novi element na kraj liste,
+D.pronalazi element u listi(po prezimenu),
+E.briše odre?eni element iz liste,
+U zadatku se ne smiju koristiti globalne varijable.*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_SIZE 50
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+typedef struct _person*Position;
+typedef struct _person
+{
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	int birthyear;
+	Position next;
+} Person;
+int main()
+{
+
+
+	return 0;
+}
